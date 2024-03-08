55488a1c9b4be93eb871271a4dbf0ffdb1f8343b
Florian Paul Azim Hoberg <gyptazy@gyptazy.ch>
2024-03-08 23:22:50 +0100

    packaging: Add qualvosec_admin packaging support

---
+++ packaging/admin/packaging.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir packages
+mkdir build
+cd build
+cmake ..
+cpack -G DEB .
+cpack -G RPM .
+cp *.deb ../packages
+cp *.rpm ../packages
+cd ..
+rm -rf build
+
+echo "Packages created. Packages can be found in directory: packages"
